b8e7f3a3f66fbf5c157f3d8f48afc2f33428d22d
burkfers <finrod09@users.noreply.github.com>
2024-02-12 09:47:18 +0100

    fix year oops

--- maccel/maccel.h
+++ maccel/maccel.h
@@ -1,5 +1,5 @@
 /* Copyright 2024 burkfers (@burkfers)
- * Copyright 2023 Wimads (@wimads)
+ * Copyright 2024 Wimads (@wimads)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
